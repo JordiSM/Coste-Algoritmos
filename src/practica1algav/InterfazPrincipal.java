450a02bd7e100f5088e6f87eaf9f541d43c6594a
jotaefe95 <81982789+juanfco95@users.noreply.github.com>
2023-05-25 13:02:40 +0200

    Versión final

---
+++ src/practica1algav/InterfazPrincipal.java
@@ -0,0 +1,13 @@
+/**
+ * Practica 1 Algoritmos Avanzados - Ing Informática UIB
+ * @date 10/03/2023
+ * @author jfher, JordiSM, peremarc, MarcoMG
+ */
+package practica1algav;
+
+/**
+ * Interfaz utilizada para enviar mensajes entre modelo, vista y controlador.
+ */
+public interface InterfazPrincipal {
+    abstract public void notificar(String s);
+}
